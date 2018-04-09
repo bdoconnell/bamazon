cd85a4a17a95aad65b7b19937cf38b1b24eed867
Brian O'Connell <accelmtgcomcast.net>
2018-04-09 16:42:12 -0500

    commit again

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,67 +1,256 @@
 // Require NPM packages
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var Table = require('cli-table');
 
 // Setup connection to SQL server
 var connection = mysql.createConnection({
-	host: "localhost",
-	port: 8889,
-	// Your username
-	user: "root",
-	// Your password
-	password: "root",
-	database: "bamazonmanager"
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'bamazon_db'
 });
+
+// Set counter for total number of products
+var numberOfProductTypes = 0;
+
 // Connect to DB
-connection.connect(function (err) {
-	if (err) throw err;
-	console.log('Connected as id' + connection.threadId);
-	startBuying();
+connection.connect(function(err) {
+    // Throw error if it errors
+    if (err) throw err;
+    // New promise that selects all data from the table
+    new Promise(function(resolve, reject) {
+        connection.query('SELECT * FROM products', function(err, res) {
+            if (err) reject(err);
+            resolve(res);
+            console.log('Welcome manager!')
+        });
+    }).then(function(result) {
+        // increment number of products based on DB
+        result.forEach(function(item) {
+            numberOfProductTypes++;
+        });
+
+        return enterManagerApp();
+        // catch errors
+    }).catch(function(err) {
+        console.log(err);
+    });
 });
-//Setting up managers prompts
-var startBuying = function () {
-	connection.query('SELECT * FROM products', function (err, res) {
 
-		inquirer
-			.prompt([{
+// Enter the manager prompt system
+function enterManagerApp() {
+    inquirer.prompt([{
+        name: 'entrance',
+        message: 'What would you like to do?',
+        type: 'list',
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'EXIT']
+    }]).then(function(answer) {
+        switch (answer.entrance) {
+            case 'View Products for Sale':
+                itemsForSale();
+                break;
+            case 'View Low Inventory':
+                lowInventory();
+                break;
+            case 'Add to Inventory':
+                addInventory();
+                break;
+            case 'Add New Product':
+                addProduct();
+                break;
+            case 'EXIT':
+                console.log('Goodbye manager!');
+                connection.destroy();
+                return;
+                break;
+            default:
+                enterManagerApp();
+                break
+        };
+    });
+}
 
-				type: "list",
-				name: "managerChoice",
-				message: "What information would you like to review?",
-				choices: ["Products for sale", "stoch_quantity<=5", "Add inventory to existing product", "Add a new product"]
+// Logs all items
+function logItems(result) {
+    result.forEach(function(item) {
+        numberOfProductTypes++;
+        console.log('Item ID: ' + item.item_id + ' || Product Name: ' + item.product_name + ' || Department: ' + item.department_name + ' || Price: ' + item.price + ' || Stock: ' + item.stock_quantity);
+    });
+}
 
-				//Set up the cli-table
-			}]).then(function (printStuff) {
-				var table = new Table({
-					head: ['Item ID', 'Product Name', 'Department', 'Cost', 'Stock']
-					, colWidths: [10, 45, 40, 8, 8]
-				});
-				for (var i = 0; i < res.length; i++) {
-					table.push([res[i].itemID, res[i].product_name, res[i].department_name, res[i].item_cost, res[i].stock_quantity]);
-				}
-				console.log(table.toString());
+// Grabs all items for sale from DB
+function itemsForSale() {
+    return new Promise(function(resolve, reject) {
+        connection.query('SELECT * FROM products', function(err, res) {
+            if (err) reject(err);
+            resolve(res);
+        });
+    }).then(function(result) {
+        logItems(result);
+    }).then(function() {
+        enterManagerApp();
+        // catch errors
+    }).catch(function(err) {
+        console.log(err);
+        connection.destroy();
+    });
+}
 
-				//Table for low inventory
-				connection.query('SELECT * FROM products, WHERE stock_quantity<=5;', function (err, res) {
-					inquirer.prompt([{
-						name: 'view',
-						type: 'confirm',
-						message: '\nWould you like to view products with inventory under 5 items?'
+// Grabs all items with an inventory below 5 only
+function lowInventory() {
+    return new Promise(function(resolve, reject) {
+        connection.query('SELECT * FROM products WHERE stock_quantity < 5', function(err, res) {
+            if (err) reject(err);
+            resolve(res);
+        });
+    }).then(function(result) {
+        logItems(result);
+    }).then(function() {
+        enterManagerApp();
+        // catch errors
+    }).catch(function(err) {
+        console.log(err);
+        connection.destroy();
+    });
+}
 
-					}]).then(function (printStuff) {
-						var table = new Table({
-							head: ['Product Name', 'Stock']
-							, colWidths: [25, 25]
-						});
-						for (var i = 0; i < res; i++) {
-							table.push([res[i].product_name, res[i].stock_quantity]);
-							choiceArray.push(res[i].product_name);
-						}
-						console.log(table.toString());
-					})
-				})
+// Function to add inventory to SQL DB
+function addInventory() {
+    return inquirer.prompt([{
+        name: 'item',
+        message: 'Enter the item number of the product you would like to add stock to.',
+        type: 'input',
+        validate: function(value) {
+            // Validator to ensure the product number is a number and it exists
+            if ((isNaN(value) === false) && (value <= numberOfProductTypes)) {
+                return true;
+            } else {
+                console.log('\nPlease enter a valid item ID.');
+                return false;
+            }
+        }
+    }, {
+        name: 'quantity',
+        message: 'How much stock would you like to add?',
+        type: 'input',
+        // Validator to ensure it is number
+        validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            } else {
+                console.log('\nPlease enter a valid quantity.');
+                return false;
+            }
+        }
+    }]).then(function(answer) {
+        return new Promise(function(resolve, reject) {
+            connection.query('SELECT stock_quantity FROM products WHERE ?', { item_id: answer.item }, function(err, res) {
+                if (err) reject(err);
+                resolve(res);
+            });
+        }).then(function(result) {
+            var updatedQuantity = parseInt(result[0].stock_quantity) + parseInt(answer.quantity);
+            var itemId = answer.item
+            connection.query('UPDATE products SET ? WHERE ?', [{
+                stock_quantity: updatedQuantity
+            }, {
+                item_id: itemId
+            }], function(err, res) {
+                if (err) throw err;
+                console.log('The total stock has been updated to: ' + updatedQuantity + '.');
+                enterManagerApp();
+            });
+            // catch errors
+        }).catch(function(err) {
+            console.log(err);
+            connection.destroy();
+        });
+        // catch errors
+    }).catch(function(err) {
+        console.log(err);
+        connection.destroy();
+    });
+}
 
-			})
-	})
+// Function to add a new product the DB
+function addProduct() {
+    return inquirer.prompt([{
+        name: 'product',
+        message: 'Enter the name of the product you would like to add.',
+        type: 'input',
+        // Validator to ensure it is not left blank
+        validate: function(value) {
+            if (value === '') {
+                console.log('\nPlease enter a valid name.');
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }, {
+        name: 'department',
+        message: 'Enter the name of the department where the product is located.',
+        type: 'input',
+        // Validator to ensure it is not left blank
+        validate: function(value) {
+            if (value === '') {
+                console.log('\nPlease enter a valid department name.');
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }, {
+        name: 'price',
+        message: 'Enter the price of the product.',
+        type: 'input',
+        // Validator to ensure it is a valid number
+        validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            } else {
+                console.log('\nPlease enter a valid price.');
+                return false;
+            }
+        }
+    }, {
+        name: 'quantity',
+        message: 'Enter the amount of initial stock quantity.',
+        type: 'input',
+        validate: function(value) {
+            // Validator to ensure it is a valid number
+            if (isNaN(value) === false) {
+                return true;
+            } else {
+                console.log('\nPlease enter a valid quantity.');
+                return false;
+            }
+        }
+    }]).then(function(answer) {
+        // new promise to update DB
+        return new Promise(function(resolve, reject) {
+            connection.query('INSERT INTO products SET ?', [{
+                product_name: answer.product,
+                department_name: answer.department,
+                price: answer.price,
+                stock_quantity: answer.quantity
+            }], function(err, res) {
+                if (err) reject(err);
+                resolve(res);
+            });
+            // log message
+        }).then(function() {
+            console.log('The product has been added to the inventory.');
+            enterManagerApp();
+            // catch errors
+        }).catch(function(err) {
+            console.log(err);
+            connection.destroy();
+        });
+        // catch errors
+    }).catch(function(err) {
+        console.log(err);
+        connection.destroy();
+    });
 }
---
+++ bamazon_db.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS bamazon_db;
+    CREATE DATABASE bamazon_db;
+    USE bamazon_db;    
+    
+    CREATE TABLE products(
+      itemID INT(5) NOT NULL AUTO_INCREMENT,
+      product_name VARCHAR(25) NOT NULL,
+      department_name VARCHAR(25) NOT NULL,
+      item_cost INT default 0,
+      stock_quantity INT default 0,
+      PRIMARY KEY (itemID)
+    );
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("TCL", "Televisions", 500, 10);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("RCA", "Televisions",400, 5);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("Samsung", "Televisions", 1500, 10);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("Panasonic", "Televisions", 2500, 7);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("Insignia", "Televisions", 500, 3);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("Sony", "Televisions", 750, 20);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("Magnovox", "Televisions", 100, 2);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("Asus", "Televisions", 550, 6);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("Mitsubishi", "Televisions", 1200, 8);
+    
+    INSERT INTO products (product_name, department_name, item_cost, stock_quantity)
+	VALUES ("Toshiba", "Televisions", 800, 12);
+    
+    SELECT * FROM products;
